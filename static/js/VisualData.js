7d985c30473d860d5515c8291a4ea2424295f9f7
TakesxiSximada <takesxi.sximada@gmail.com>
2015-05-20 14:36:28 +0900

    add super rainbow

--- static/js/VisualData.js
+++ static/js/VisualData.js
@@ -6,26 +6,41 @@
 
     var VisualData = {
         version: "0.1.0",
-        tutorial_selection: function (tree){
-            // all red
-            var paragraphs = tree.getElementsByTagName('p');  // HTMLCollection object
-            Array.prototype.forEach.call(paragraphs, function (paragraph){
-                paragraph.style.setProperty('color', 'red', null);
+
+        // selection
+        draw: function(target, value, selector, tree){
+            if (typeof tree === 'undefined') tree = document;
+            var elms = tree.getElementsByTagName(selector);
+            Array.prototype.forEach.call(elms, function (elm){
+                elm.style.setProperty(target, value, null);
             });
-
-            // all yellow
-            d3.selectAll('p').style('color', 'yellow');
-
-            // body black
-            d3.select('body').style('background-color', 'black');
         },
-        tutorial_dynamic_properties: function (){
-            // rainbow
-            d3.selectAll('p').style('color', function(){
+
+        // dynamic properties
+        rainbow: function (selector){
+            d3.selectAll(selector).style('color', function(){
                 return 'hsl(' + Math.random() * 360 + ', 100%, 50%)';
             });
-            // zabra
-            d3.selectAll('p').style('color', function(d, i){
+        },
+
+        super_rainbow: function (selector){
+            var elms = d3.selectAll(selector);
+            Array.prototype.forEach.call(elms, function (elm){
+                var elm = elm[0];
+                var data = elm.textContent;
+                elm.textContent = '';
+                Array.prototype.forEach.call(data, function (ch){
+                    var tag = document.createElement('span');
+                    tag.textContent = ch;
+                    d3.select(tag).style('color', 'hsl(' + Math.random() * 360 + ', 100%, 50%)');
+                    elm.appendChild(tag);
+                });
+            });
+        },
+
+        // dynamic properties
+        zebra: function (selector){
+            d3.selectAll(selector).style('color', function(d, i){
                 return i % 2 ? "#115" : "#3cc";
             });
         },
